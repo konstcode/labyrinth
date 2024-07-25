edd332618606d4c87377872e90faa23351e4caa2
Kostinatyn Konakhevych <kostiantyn.konakhevych@gmail.com>
2024-07-25 14:22:28 +0300

    Engine configured

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use rusty_engine::prelude::*;
+
+#[derive(Resource)]
+struct GameState {}
+
 fn main() {
-    println!("Hello, world!");
+    let mut game = Game::new();
+    let game_state = GameState {};
+
+    game.add_logic(game_logic); // Don't forget to add the logic function to the game!
+    game.run(game_state);
 }
+
+fn game_logic(engine: &mut Engine, game_state: &mut GameState) {}
