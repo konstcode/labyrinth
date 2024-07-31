c7c028aa211b14d0166ecac6dc38db7e70e4c956
Kostinatyn Konakhevych <kostiantyn.konakhevych@gmail.com>
2024-07-31 13:41:16 +0300

    basic movments depends on titling, speed limits, titling stop

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # Labyrinth
+
+Main description [here](https://github.com/CleanCut/rusty_engine/blob/main/scenarios/labrinth.md).
+
+## Concept
+
+* In one screen we can see rounded or square labyrinth(possible few levels/maps). There are also have start zone 
+  where the ball/marble located. When we tilting field with mouse or possible with arrow keys or wasd, ball 
+  start roll to the direction of tilting.
+* Labyrinth consist of walls that marble collide with and can't pass through.
+* Speed and direction depends on angle and angle of tilting of map/field.
+* Possible task is auto-generation of labyrinth with some graph theory.
+
+## Objects
+
+* Board - a field where all happen that can be tilted and have animation of shadowing or 3D like perspective.
+* Wall - prevent marble to pass through on collision and have same 3D effect as board.
+* Marble - have speed, acceleration, direction of roll depends on board tilting.
+* StartPoint - where marble appears at the beginning.
+* FinishPoint - win if marble got there.
+* Hole - if marble collide with it - game over.
+
+## Investigation #todo
+
+- [x] **rusty engine** - simplified game engine, wrapper on real **bevy**. Configure basic setup and make something simple.
+- [ ] move marble while tilting board: speed, acceleration/deceleration, direction.
+- [ ] **Collision contact formal**'s & and calculate collision vector of marble and wall.
+- [ ] hole and marble interaction. Determine the center of sprite(hole). If one spite touch/collide center of other event.
+
+### Marble + Board logic and build steps
+
+Mouse control ~ Board tilting ~ F(Marble speed + direction):
+* Center in (0, 0) and radius of save zone for mouse 50 to avoid over moving
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,78 @@
+use log::{debug, warn};
 use rusty_engine::prelude::*;
 
 #[derive(Resource)]
-struct GameState {}
+struct GameState {
+    // board tilting in some direction
+    tilting_vec: Vec2,
+    // marble have own move direction
+    marble_vec: Vec2,
+    marble_center: Vec2,
+}
+
+fn direction_from_vector(vec: Vec2) -> f32 {
+    vec.y.atan2(vec.x)
+}
+
+fn translate_titling_to_marble(tilting_vec: Vec2, marble_center: Vec2) -> Vec2 {
+    let translation = marble_center;
+    tilting_vec + translation
+}
+
+const MARBLE_RATE: f32 = 50.0;
+const MAX_TITLING: f32 = 500.0;
+const MIN_TITLING: f32 = 50.0;
 
 fn main() {
+    env_logger::init();
     let mut game = Game::new();
-    let game_state = GameState {};
+    let game_state = GameState {
+        tilting_vec: Vec2::new(0.0, 0.0),
+        marble_vec: Vec2::new(0.0, 0.0),
+        marble_center: Vec2::new(200.0, 300.0),
+    };
 
-    game.add_logic(game_logic); // Don't forget to add the logic function to the game!
+    let _ = game.add_sprite("marble", SpritePreset::RollingBallBlue);
+    let marble = game.sprites.get_mut("marble").unwrap();
+    marble.translation = game_state.marble_vec;
+
+    let _ = game.add_sprite("hole", SpritePreset::RollingHoleStart);
+    let center = game.sprites.get_mut("hole").unwrap();
+    center.translation = Vec2::new(0.0, 0.0);
+
+    game.add_logic(game_logic);
     game.run(game_state);
 }
 
-fn game_logic(engine: &mut Engine, game_state: &mut GameState) {}
+fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
+    let marble = engine.sprites.get_mut("marble").unwrap();
+
+    if let Some(location) = engine.mouse_state.location() {
+        if location.length() < MIN_TITLING {
+            game_state.tilting_vec = Vec2::new(0.0, 0.0);
+        } else if location.x > MAX_TITLING {
+            game_state.tilting_vec.x = location.x / location.x.abs() * MAX_TITLING / MARBLE_RATE;
+        } else if location.y > MAX_TITLING {
+            game_state.tilting_vec.y = location.y / location.y.abs() * MAX_TITLING / MARBLE_RATE;
+        } else {
+            game_state.tilting_vec = location / MARBLE_RATE;
+        }
+    }
+
+    game_state.marble_vec =
+        translate_titling_to_marble(game_state.tilting_vec, game_state.marble_center);
+
+    let rotate_to = direction_from_vector(game_state.tilting_vec);
+    marble.rotation = if rotate_to != 0.0 {
+        rotate_to
+    } else {
+        marble.rotation
+    };
+    marble.translation = game_state.marble_vec
+        + engine.delta_f32 * (game_state.marble_center - game_state.marble_vec);
+    game_state.marble_center = game_state.marble_vec;
+
+    if marble.translation.length() > engine.window_dimensions.length() {
+        game_state.marble_center = Vec2::new(100.0, 200.0);
+    }
+}
